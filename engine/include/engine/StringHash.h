ebba9d06a97ec1925d3ea596ee9f8d593a9ee244
chanu <chanuka.williams@outlook.com>
2026-01-04 15:13:18 +0000

    Added equality checks with string_views

--- engine/include/engine/StringHash.h
+++ engine/include/engine/StringHash.h
@@ -6,7 +6,18 @@ struct StringHash
 {
     using is_transparent = void;  // Enable heterogeneous lookup
     
-    [[nodiscard]] size_t operator()(const std::string_view sv) const noexcept {
+    [[nodiscard]] size_t operator()(const std::string_view sv) const noexcept
+    {
         return std::hash<std::string_view>{}(sv);
+    }
+};
+
+struct StringEqual
+{
+    using is_transparent = void;
+
+    [[nodiscard]] bool operator()(const std::string_view lhs, const std::string_view rhs) const noexcept
+    {
+        return lhs == rhs;
     }
 };
