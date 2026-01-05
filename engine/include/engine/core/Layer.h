50c748315cceab3c51c9a239d65e5cfafc6e8b2f
chanu <chanuka.williams@outlook.com>
2026-01-05 15:07:56 +0000

    Layers can now have different states and visibilities.

--- engine/include/engine/core/Layer.h
+++ engine/include/engine/core/Layer.h
@@ -3,6 +3,18 @@
 namespace engine::core
 {
 
+enum class LayerState
+{
+    ACTIVE,
+    PAUSED
+};
+
+enum class LayerVisibility
+{
+    VISIBLE,
+    HIDDEN
+};
+
 class Layer
 {
 public:
@@ -11,6 +23,16 @@ public:
 
     virtual void OnUpdate() = 0;
     virtual void OnRender() = 0;
+
+    [[nodiscard]] LayerState GetState() const;
+    void SetState(LayerState state);
+
+    [[nodiscard]] LayerVisibility GetVisibility() const;
+    void SetVisibility(LayerVisibility visibility);
+
+private:
+    LayerState m_state = LayerState::ACTIVE;
+    LayerVisibility m_visibility = LayerVisibility::VISIBLE;
 };
 
 } // namespace engine::core
