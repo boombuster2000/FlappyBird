7fc98c4e33ed5dc0ff90e07764705fc05da659ff
chanu <chanuka.williams@outlook.com>
2026-01-21 18:13:44 +0000

    Added functionality to trigger transition to ball layer

--- game/src/layers/MainMenuLayer.cpp
+++ game/src/layers/MainMenuLayer.cpp
@@ -1,9 +1,13 @@
 #include "game/layers/MainMenuLayer.h"
 
+#include "engine/core/ServiceLocator.h"
+#include "game/layers/BouncingBallLayer.h"
 #include "raylib.h"
 
 void MainMenuLayer::OnUpdate()
 {
+    if (IsKeyPressed(KEY_TWO))
+        engine::core::ServiceLocator::GetApplication()->QueueLayerTransition<MainMenuLayer, BouncingBallLayer>();
 }
 
 void MainMenuLayer::OnRender()
